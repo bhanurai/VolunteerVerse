078a8819425d83d5f5c0e2c6c1a3e577dfd57be7
Bhanu Rai <bhanurai001@gmail.com>
2024-07-15 13:31:14 +0545

    detail

--- lib/features/global/featured_organization/detail_page.dart
+++ lib/features/global/featured_organization/detail_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class PrayasNepalApp extends StatelessWidget {
   final String? organizationName;
 
-  const PrayasNepalApp({Key? key, required this.organizationName}) : super(key: key);
+  const PrayasNepalApp({Key? key, required this.organizationName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +52,21 @@ class PrayasNepalApp extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 16.0),
-                      Text(
-                        'Prayatnashil Community Development Society (PRAYAS-Nepal) is a non-governmental organization dedicated towards quality education, livelihood, prosperous health, good-governance, inclusive and well-being of poor and marginalized Nepalese people. Prayas or PRAYAS-Nepal focuses on children, women, marginalized and rural communities people by setting holistic development goals in creating transformed and interdependent society.\n\n'
-                        'PRAYAS-Nepal envisions a new Nepal by positive transformation of socio-economic aspect of society and individual through capacity enhancement and optimum utilization of local resources. From its inception, it works for Nepalese children for quality education to produce quality lives and families for safe environment with their dignity, respect, food security, income generating, health or security regardless of their gender, ethnicity and religion.\n\n'
-                        'PRAYAS-Nepal is registered with District Administration Office of Dhading (on 2058-12-8(B.S.)) as well as Social Welfare Council (2059-9-6) since it has been working in the field of community development. It is also a member of NFN (NGO Federation Nepal). The daily activity is managed by. PRAYAS Nepal has an office located at Neelkantha 8 Dhading Besi and co-ordination and logistic support is managed at Kathmandu. The organization was founded with 9 members and now it has 31 active members.',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Prayatnashil Community Development Society (PRAYAS-Nepal) is a non-governmental organization dedicated towards quality education, livelihood, prosperous health, good-governance, inclusive and well-being of poor and marginalized Nepalese people. Prayas or PRAYAS-Nepal focuses on children, women, marginalized and rural communities people by setting holistic development goals in creating transformed and interdependent society.',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'PRAYAS-Nepal envisions a new Nepal by positive transformation of socio-economic aspect of society and individual through capacity enhancement and optimum utilization of local resources. From its inception, it works for Nepalese children for quality education to produce quality lives and families for safe environment with their dignity, respect, food security, income generating, health or security regardless of their gender, ethnicity and religion.',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'PRAYAS-Nepal is registered with District Administration Office at Dhading (on 2058-12-6B.S.) as well as Social welfare Council (2059-9-6). Since then it has been working in the field of community development. It is also a member of NGO federation Nepal. The daily activity is managed by. PRAYAS Nepal has an office located at Neelakantha 5 Dhading Bensi and co-ordination and logistic support unit is being established at Kathamandu. The organization was founded with 9 members and now it has 31 active members. The organization has 7-member executive committee. It has 17 permanent staff members and other project staff.',
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -71,7 +83,8 @@ class PrayasNepalApp extends StatelessWidget {
 class BabylifeHomePage extends StatelessWidget {
   final String? organizationName;
 
-  const BabylifeHomePage({Key? key, required this.organizationName}) : super(key: key);
+  const BabylifeHomePage({Key? key, required this.organizationName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -119,10 +132,25 @@ class BabylifeHomePage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 16.0),
-                      Text(
-                        'Prayatnashil Community Development Society (PRAYAS-Nepal) is a non-governmental organization dedicated towards quality education, livelihood, prosperous health, good-governance, inclusive and well-being of poor and marginalized Nepalese people. Prayas or PRAYAS-Nepal focuses on children, women, marginalized and rural communities people by setting holistic development goals in creating transformed and interdependent society.\n\n'
-                        'PRAYAS-Nepal envisions a new Nepal by positive transformation of socio-economic aspect of society and individual through capacity enhancement and optimum utilization of local resources. From its inception, it works for Nepalese children for quality education to produce quality lives and families for safe environment with their dignity, respect, food security, income generating, health or security regardless of their gender, ethnicity and religion.\n\n'
-                        'PRAYAS-Nepal is registered with District Administration Office of Dhading (on 2058-12-8(B.S.)) as well as Social Welfare Council (2059-9-6) since it has been working in the field of community development. It is also a member of NFN (NGO Federation Nepal). The daily activity is managed by. PRAYAS Nepal has an office located at Neelkantha 8 Dhading Besi and co-ordination and logistic support is managed at Kathmandu. The organization was founded with 9 members and now it has 31 active members.',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Mr. Rajkumar Pun and Mrs. Uma Gurung led to the establishment of Baby Life Home in the year 2067 B.S. (2009 A.D).',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Baby life home is primarily focused on the rescuing of children from districts of: Dang, Baitadi, Kailali, Salyan, Dailekh, Surkhet, Rolpa and Kanchanpur where these children were obliged to live tedious and miserable lives in helpless situations under persistent social disintegration and discrimination.',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'After their rescue, the children of Baby Life Home/Saphalta HIV Shikshya Sadan now feel more confident, capable and inspired. They are also much, much healthier and almost all appear no different than any other child (except the deep understandings, emotions and even wisdom and compassion one can see and feel when looking into their eyes).',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Baby life Home excels at giving loving parentage to neglected children after the demise of their parents and rejection of their families and society. Our organization has been active in rescue-programs for the basic needs, formal education, and healthcare of these remarkable children.',
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -139,7 +167,8 @@ class BabylifeHomePage extends StatelessWidget {
 class MaitiNepal extends StatelessWidget {
   final String? organizationName;
 
-  const MaitiNepal({Key? key, required this.organizationName}) : super(key: key);
+  const MaitiNepal({Key? key, required this.organizationName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -187,10 +216,21 @@ class MaitiNepal extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 16.0),
-                      Text(
-                        'Prayatnashil Community Development Society (PRAYAS-Nepal) is a non-governmental organization dedicated towards quality education, livelihood, prosperous health, good-governance, inclusive and well-being of poor and marginalized Nepalese people. Prayas or PRAYAS-Nepal focuses on children, women, marginalized and rural communities people by setting holistic development goals in creating transformed and interdependent society.\n\n'
-                        'PRAYAS-Nepal envisions a new Nepal by positive transformation of socio-economic aspect of society and individual through capacity enhancement and optimum utilization of local resources. From its inception, it works for Nepalese children for quality education to produce quality lives and families for safe environment with their dignity, respect, food security, income generating, health or security regardless of their gender, ethnicity and religion.\n\n'
-                        'PRAYAS-Nepal is registered with District Administration Office of Dhading (on 2058-12-8(B.S.)) as well as Social Welfare Council (2059-9-6) since it has been working in the field of community development. It is also a member of NFN (NGO Federation Nepal). The daily activity is managed by. PRAYAS Nepal has an office located at Neelkantha 8 Dhading Besi and co-ordination and logistic support is managed at Kathmandu. The organization was founded with 9 members and now it has 31 active members.',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Maiti is a beautiful word from the Nepali language, meaning a woman’s childhood home. Apart from being a beautiful word that evokes memories and a feeling of comfort, Maiti is a concept that many Nepali women carry close to their hearts.',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Founded in 1993, Maiti Nepal carries the same concept and space for women that they bear close to their heart for the word ‘Maiti.’ Maiti Nepal is the place where women who have been trafficked, exploited, abused, or had their rights denied in any form can find a home for themselves. Headquartered in Kathmandu, Maiti Nepal is a non-profit, secular organization working to protect and improve the lives of vulnerable individuals and rescue survivors. The organization promotes children’s and women’s rights and aims to put an end to trafficking through these key activity sectors: 2A’s advocacy and awareness, 6P’s prevention, protection, participation, prosecution, partnership and policy, 5R’s rescue, repatriation, rehabilitation, reintegration and referral. It seeks to build awareness and secure cooperation from schools and college students, parliamentarians, federal, provincial, and local governments, law enforcement agencies and organizations in Nepal, and their counterparts in India.',
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Maiti Nepal, today, conducts a wide range of activities. Organizing awareness campaigns, community sensitization programs, rescue operations, apprehending traffickers, providing legal support to the needy, women empowerment programs, training, providing antiretroviral therapy (ART) to children and women infected by HIV are regular activities of the organization.',
+                          ),
+                        ],
                       ),
                     ],
                   ),
